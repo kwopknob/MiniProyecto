887aa64f98d3be69bec3bcd18793d9aab950555f
kwopknob <kwopknob@gmail.com>
2025-02-16 20:58:57 +0100

    AñadidoAim y cambiado textura

--- Assets/AimStates/AimStateManager.cs
+++ Assets/AimStates/AimStateManager.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AimState : MonoBehaviour
+public class AimStateManager : MonoBehaviour
 {
+
+    AimBaseState currentState;
+    public HipfireState Hip = new HipfireState();
+    public AimState Aim = new AimState();
     [SerializeField] float mouseSense = 1;
     float xAxis, yAxis;
     [SerializeField] Transform camFollowPos;
+
+    [HideInInspector] public Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        SwitchState(Hip);
+    }
 
     private void Update()
     {
         xAxis += Input.GetAxisRaw("Mouse X") * mouseSense;
         yAxis -= Input.GetAxisRaw("Mouse Y") * mouseSense ;
         yAxis = Mathf.Clamp(yAxis, -80, 80);
+        currentState.UpdateState(this);
 
     }
 
@@ -20,5 +33,11 @@ public class AimState : MonoBehaviour
     {
         camFollowPos.localEulerAngles = new Vector3(yAxis, camFollowPos.localEulerAngles.y, camFollowPos.localEulerAngles.z);
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis, transform.eulerAngles.z);
+    }
+
+    public void SwitchState(AimBaseState state)
+    {
+        currentState = state;
+        currentState.EnterState(this);
     }
 }
